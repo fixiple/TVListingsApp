56a2da9ff6b9132d81217f71faa1c699d66aeb54
fixiple <djessy.engo47@gmail.com>
2024-02-03 21:00:00 +0100

    worked on how to get data from API

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,20 +1,64 @@
 import { Component } from '@angular/core';
 import API  from '../service/API.service';
 import { CommonModule } from '@angular/common';
+import { map} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
+  providers: [API],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
     response: any;
-    constructor(private API: API) {}
+
+    titles?: string[];
+    ids?: number[]; 
+
+    title?: string;
+    id?: number
+    constructor(private API: API) {
+        this.searchCall("The apothecary")
+    }
+
+    searchCall(req: string){
+        this.API.get(req)
+        .pipe(
+            map((data:any) => {
+                data.results.map((result:any) => {
+                    // link: https://stackoverflow.com/a/68144628
+                    this.titles?.push(result.name || result.title)
+                    //DEBUG works
+                    // console.log(result.name || result.title)
+                })
+            })
+        )
+        .subscribe(
+            (val: any) => {
+                //console.log(val)
+                        // this.titles?.push(i["name"])
+                        // this.ids?.push(i["id"])
+                    },
+            (err: any) => console.error(err),
+        )
+        
+    }
     ngOnInit() {
-        this.response=this.API.get("The Apothecary Diary")
+        // this.titles?.map(
+        //     (titleFromList:string) =>{
+        //         console.log("title number ", this.titles?.indexOf(titleFromList), ":", titleFromList)
+        //     }
+        // )
+        // this.API
+        // .get("The Apothecary Diary")
+        // .subscribe((values: any) => {
+        //     this.response=values;
+        //     console.log("HERE");
+        //     //console.log(values["page"]);
+        // })
         // console.log(this.response.toString())
     }
 }
--- front/src/app/service/API.service.ts
+++ front/src/app/service/API.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { searchResponse } from '../_interfaces/searchResponse';
 
 
 const API_KEY = environment.API_KEY;
@@ -13,29 +15,24 @@ export default class API{
     constructor(private http: HttpClient) { }
     headers=new HttpHeaders();
 
-    response: any;
+    // response: any;
     
-    public get(query_string : string, page_number : Number = 1 ) : Response {
+    public get(query_string : string, page_number : Number = 1 ) : any {
         this.headers.append('accept','application/json')
         const httpOptions = {
             headers: this.headers 
         };
         let url='https://api.themoviedb.org/3/search/multi?query='+query_string+'&page='+page_number+'&api_key='+API_KEY
-        this.http.get(
+        return this.http.get(
             url,
             httpOptions
         )
-        .subscribe({
-            //eighter using function or observer object
-            next: val => {
-                this.response = val;
-            },
-            error: error => {
-                //console.error(err);
-               console.error("error: ", error.error.status_message)
-            }
-        });
-        return this.response;
+        // .subscribe({
+        //     //eighter using function or observer object
+        //     val => this.response = val.json(),
+        //     error: (error) => console.error("error: ", error.error.status_message)
+        // });
+        // return this.response;
     }
 
     //OTHER METHOD FOUND IN TMDB DOCUMENTATION
